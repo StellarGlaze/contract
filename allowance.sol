8a7c479c809bdcdff81a8c378cca0fc4945eeeea
Om More <51229945+thisisommore@users.noreply.github.com>
2023-12-10 01:19:44 +0530

    transferAllowance: add receiver

--- allowance.sol
+++ allowance.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 contract AllowanceContract {
     address public provider;
     mapping(address => uint) public allowances;
@@ -6,13 +8,13 @@ contract AllowanceContract {
     uint public endTime;
     string public transferMessage;
     address[] public recipients;
-
+    uint256 contractBalance=0;
     event AllowanceSet(address indexed recipient, uint amount, uint startTime, uint endTime);
     event AllowanceTransferred(address indexed recipient, uint amount, string message);
     event RecipientRemoved(address indexed recipient);
     event TransferMessage(string message);
 
-    constructor(address _provider, address[] memory _recipients, uint _transferDay, uint _startTime, uint _endTime) {
+    constructor(address _provider, address[] memory _recipients, uint _transferDay, uint _startTime, uint _endTime) payable {
         provider = _provider;
         transferDay = _transferDay;
         startTime = _startTime;
@@ -22,7 +24,13 @@ contract AllowanceContract {
         for (uint i = 0; i < _recipients.length; i++) {
             allowances[_recipients[i]] = 0;
         }
+        contractBalance+=msg.value;
     }
+
+    function deposit() public payable {
+        contractBalance+=msg.value;
+    }
+
 
     function setAllowances(address[] memory _recipients, uint[] memory amounts) public {
         require(msg.sender == provider, "Only the provider can set allowances");
@@ -35,12 +43,15 @@ contract AllowanceContract {
         emit AllowanceSet(_recipients[_recipients.length - 1], amounts[amounts.length - 1], startTime, endTime);
     }
 
-    function transferAllowance() public {
+    function transferAllowance(address receiver, uint256 amount) public {
         require(block.timestamp >= startTime && block.timestamp <= endTime, "Allowance transfer not allowed at this time");
 
         for (uint i = 0; i < recipients.length; i++) {
             address recipient = recipients[i];
-            uint amount = allowances[recipient];
+            uint allowed_amount = allowances[recipient];
+            require(amount<=allowed_amount,"amount greater than allowed amount");
+            allowances[recipient]-=amount;
+            payable(receiver).transfer(amount);
             emit AllowanceTransferred(recipient, amount, transferMessage);
         }
     }
